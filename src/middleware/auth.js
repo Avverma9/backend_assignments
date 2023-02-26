ea1a103b9e20338c75d3f191179231b7fbd5cb1b
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-02-26 23:25:06 +0530

    updated

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,24 +7,22 @@ const authController = {};
 authController.signup = async (req, res) => {
   const { email, password } = req.body;
   try {
-    // Check if user already exists
+  
     const user = await User.findOne({ email });
     if (user) {
       return res.status(409).json({ message: 'User already exists' });
     }
     
-    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create new user
-    const newUser = new User({
+  const newUser = new User({
       email,
       password: hashedPassword
     });
 
     await newUser.save();
 
-    // Create token
+   
     const token = jwt.sign(
       { email: newUser.email, userId: newUser._id },
       process.env.JWT_SECRET,
@@ -42,19 +40,18 @@ authController.login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Check if user exists
+  
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
 
-    // Compare passwords
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
 
-    // Create token
+    
     const token = jwt.sign(
       { email: user.email, userId: user._id },
       process.env.JWT_SECRET,
